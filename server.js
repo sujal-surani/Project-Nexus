cd906f204a8a88b3c427bf48096bc2f5e8455ba3
sujal-hp <suranisujal19@gmail.com>
2026-09-02 10:35:11 +0530

    fix: typo in server.js - .hmtl

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/api/health', (req, res) =>{
 
 
 app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname,'public', 'index.hmtl'));
+    res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
 app.listen(port,'0.0.0.0', () => console.log(`Nexus api running on port ${port}`));
